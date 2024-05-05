946907484e5f78474dbd255f69cab9f94a1d56a7
hsilasss <hsilassamban@gmail.com>
2024-05-06 05:04:05 +0700

    tp 11 library

---
+++ AljabarLibrary/Class1.cs
@@ -0,0 +1,30 @@
+﻿namespace AljabarLibrary
+{
+    public class Class1
+    {
+        public static double[] AkarPersamaanKuadrat(double a, double b,double c)
+        {
+          
+          double  persamaan = Math.Pow(b, 2) - 4 * a * c;
+
+            if (persamaan >= 0)
+            {
+                double root1 = (-b + Math.Sqrt(persamaan)) / (2 * a);
+                double root2 = (-b - Math.Sqrt(persamaan)) / (2 * a);
+                return new double[] { root1, root2 };
+            }
+            else
+            {
+                return new double[] { };
+            }
+        }
+        public static double[] HasilKuadrat(double a, double b)
+        {
+            double aKuadrat = Math.Pow(a, 2);
+            double duaAB = 2 * a * b;
+            double bKuadrat = Math.Pow(b, 2);
+            return new double[] {aKuadrat, duaAB, bKuadrat };
+        }
+    }
+}
+
---
+++ AljabarMain/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AljabarLibrary;
+
+double[] hasilAkar = Class1.AkarPersamaanKuadrat(2, 4, 8);
+Console.WriteLine("Hasil Akar: {0} {1}", hasilAkar[0],  hasilAkar[1]);
+
+double[] hasilKuadrat = Class1.HasilKuadrat(3, 6);
+Console.WriteLine("hasil kuadrat: {0}x^2 + {1}x + {2}", hasilKuadrat[0], hasilKuadrat[1], hasilKuadrat[2]);
